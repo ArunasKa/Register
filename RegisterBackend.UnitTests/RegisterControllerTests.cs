37221171aa0f895a2a4b3493f36d5b7bf12e3025
Arunas <66676626+ArunasKa@users.noreply.github.com>
2022-11-21 18:59:59 +0200

    Added test for delete

--- RegisterBackend.UnitTests/RegisterControllerTests.cs
+++ RegisterBackend.UnitTests/RegisterControllerTests.cs
@@ -15,79 +15,79 @@ namespace RegisterBackend.UnitTests
         public RegisterControllerTests()
         {
             _registerServiceMock = new Mock<IRegisterService>();
-            _sut = new RegisterController( _registerServiceMock.Object);
+            _sut = new RegisterController(_registerServiceMock.Object);
         }
         [Fact]
         public async Task UpdateName_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdateName(testId, "Name")as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdateName(testId, "Name") as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdateName_WhenPersonNotFound_ReturnNull()
         {
-            var result = await _sut.UpdateName(testId, "Name")as StatusCodeResult;
-            Assert.Equal(null, result );
+            var result = await _sut.UpdateName(testId, "Name") as StatusCodeResult;
+            Assert.Equal(null, result);
         }
         [Fact]
         public async Task UpdateLastName_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdateLastName(testId, "Lastname")as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdateLastName(testId, "Lastname") as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdateLastName_WhenPersonNotFound_ReturnNull()
         {
             var result = await _sut.UpdateLastName(testId, "Lastname") as StatusCodeResult;
-            Assert.Equal(null, result );
+            Assert.Equal(null, result);
         }
         [Fact]
         public async Task UpdatePersonalCode_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdatePersonalCode(testId, 1)as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdatePersonalCode(testId, 1) as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdatePersonalCode_WhenPersonNotFound_ReturnNull()
         {
             var result = await _sut.UpdatePersonalCode(testId, 1) as StatusCodeResult;
-            Assert.Equal(null, result );
+            Assert.Equal(null, result);
         }
         [Fact]
         public async Task UpdatePhoneNumber_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdatePhoneNumber(testId, 1)as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdatePhoneNumber(testId, 1) as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdatePhoneNumber_WhenPersonNotFound_ReturnNull()
         {
             var result = await _sut.UpdatePhoneNumber(testId, 1) as StatusCodeResult;
-            Assert.Equal(null, result );
+            Assert.Equal(null, result);
         }
         [Fact]
         public async Task UpdateEmail_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdateEmail(testId, "Email")as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdateEmail(testId, "Email") as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdateEmail_WhenPersonNotFound_ReturnNull()
         {
             var result = await _sut.UpdateEmail(testId, "Email") as StatusCodeResult;
-            Assert.Equal(null, result );
+            Assert.Equal(null, result);
         }
         [Fact]
         public async Task UpdatePhoto_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdatePhoto(testId, new ImageDto { })as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdatePhoto(testId, new ImageDto { }) as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdatePhoto_WhenPersonNotFound_ReturnNull()
@@ -99,8 +99,8 @@ namespace RegisterBackend.UnitTests
         public async Task UpdateCity_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdateCity(testId, "City")as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdateCity(testId, "City") as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdateCity_WhenPersonNotFound_ReturnNull()
@@ -112,8 +112,8 @@ namespace RegisterBackend.UnitTests
         public async Task UpdateStreetName_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdateStreetName(testId, "Street name")as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdateStreetName(testId, "Street name") as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdateStreetName_WhenPersonNotFound_ReturnNull()
@@ -125,8 +125,8 @@ namespace RegisterBackend.UnitTests
         public async Task UpdateHouseNumber_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdateHouseNumber(testId, "House number")as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdateHouseNumber(testId, "House number") as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdateHouseNumber_WhenPersonNotFound_ReturnNull()
@@ -138,8 +138,8 @@ namespace RegisterBackend.UnitTests
         public async Task UpdateApartmentNumber_WhenPersonFound_ReturnOkCodeResult()
         {
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(new Person { });
-            var result = await _sut.UpdateApartmentNumber(testId, "Apartment number")as StatusCodeResult;
-            Assert.Equal(200, result.StatusCode );
+            var result = await _sut.UpdateApartmentNumber(testId, "Apartment number") as StatusCodeResult;
+            Assert.Equal(200, result.StatusCode);
         }
         [Fact]
         public async Task UpdateApartmentNumber_WhenPersonNotFound_ReturnNull()
@@ -153,7 +153,7 @@ namespace RegisterBackend.UnitTests
 
             var testPerson = new Person
             {
-                Name ="testName"
+                Name = "testName"
             };
             _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(testPerson);
             var result = await _sut.GetAllInfoById(testId);
@@ -176,7 +176,7 @@ namespace RegisterBackend.UnitTests
                 ImageBytes = new byte[0]
 
             };
-            _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(testPerson );
+            _registerServiceMock.Setup(w => w.GetPersonByIdAsync(testId)).ReturnsAsync(testPerson);
             var result = await _sut.DownloadImage(testId) as FileContentResult;
             Assert.Equal(testPerson.ImageBytes, result.FileContents);
         }
@@ -188,5 +188,25 @@ namespace RegisterBackend.UnitTests
             Assert.Equal("No user by id", actual.Value);
 
         }
-    }
+        [Fact]
+        public async Task DeleteUser_WhenUserIsFound_ReturnImage()
+        {
+            var testPerson = new UserAccount
+            {
+                UserName ="TestUsername",
+
+            };
+            _registerServiceMock.Setup(w => w.GetUserIdAsync(testId)).ReturnsAsync(testPerson);
+            var result = await _sut.Delete(testId) as ObjectResult;
+            Assert.Equal("User Deleted", result.Value);
+        }
+        [Fact]
+        public async Task DeleteUser_WhenUserIsNotFound_ReturnImage()
+        {
+            var result = await _sut.Delete(testId);
+            var actual = result as ObjectResult;
+            Assert.Equal("No user by id", actual.Value);
+
+        }
+    } 
 }
